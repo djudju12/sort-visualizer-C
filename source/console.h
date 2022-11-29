4a4a651e3fe24caf7a71a9a0e97f9372f4933c4c
djudju12 <jonathan.willian321@gmail.com>
2022-11-29 00:25:13 -0300

    ultimaAtt

--- source/console.h
+++ source/console.h
@@ -1,42 +1,21 @@
-#ifndef CONSOLE_H
-#define CONSOLE_H
+#ifndef CONSOLE_H_INCLUDED
+#define CONSOLE_H_INCLUDED
+
+#include <windows.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 //retorna o tamanho do console
-void get_size_window(int *col, int *row){
-    CONSOLE_SCREEN_BUFFER_INFO cmd;
-
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cmd);
-    *col = cmd.srWindow.Right - cmd.srWindow.Left +1;
-    *row = cmd.srWindow.Bottom - cmd.srWindow.Top +1;
-}
+void get_size_window(int *col, int *row);
 
 //move o cursor para a coordenada X, Y
-void GotoXY(int x, int y){
-    HANDLE a;
-    COORD b;
-    fflush(stdout);
-    b.X = x;
-    b.Y = y;
-    a = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleCursorPosition(a,b);
-}
+void GotoXY(int x, int y);
 
 //maxima a tela no inicio do programa
-void maximize_window(){
-    HWND consoleWindow = GetConsoleWindow();
-    ShowWindow(consoleWindow, SW_MAXIMIZE);
-}
+void maximize_window();
 
 //esconde o cursor
-void ShowConsoleCursor(bool showFlag)
-{
-    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    CONSOLE_CURSOR_INFO     cursorInfo;
-
-    GetConsoleCursorInfo(out, &cursorInfo);
-    cursorInfo.bVisible = showFlag; // set the cursor visibility
-    SetConsoleCursorInfo(out, &cursorInfo);
-}
+void ShowConsoleCursor(bool showFlag);
 
 #endif
--- source/linkedlist.h
+++ source/linkedlist.h
@@ -1,163 +1,41 @@
 #ifndef LINKEDLIST_H_INCLUDED
 #define LINKEDLIST_H_INCLUDED
 
-typedef struct node{
-    int data;
-    struct node *next;
-}node;
-node *head;
+#include <stdlib.h>
+#include <stdio.h>
 
+struct node *head;
 //----OPERAÇOES LINKED LIST----//
 
 //recebe um vetor e converte para uma linked list
-void copy(int array[], int tamanho)
-{
-    node *temp = malloc(sizeof(node));
-    temp->data=array[0];
-    temp->next=NULL;
-    head =temp;
-    int i;
-    for(i=1;i<tamanho;i++)
-    {
-        node*temp2= malloc(sizeof(node));
-        temp->next= temp2;
-        temp2->data = array[i];
-        temp2->next = NULL;
-        temp=temp2;
-     }  
-}
+void copy(int array[], int tamanho);
 
 //percorre a lista
-void percorrer()
-{
-	node *temp;
-	
-	temp = head;
-	while(temp != NULL){
-		temp = temp->next;
-	}	
-}
+void percorrer();
 
-void printlist() {
-    node*temp = head;
-
-	while(temp!= NULL){
-		printf(" %d ",temp->data);
-		temp=temp->next;
-    }
-    printf("\n");
-}
+void printlist();
 
 //remove o primeiro elemento da lista
-void removerPrimeiro()
-{
-	node *temp;
-	
-	temp = head;
-	head = head->next;
-	free(temp);
-}
+void removerPrimeiro();
 
 //remove um elemento em dada posicao
-void remover(int index)
-{
-	node *temp, *posicao;
-	int i = 0;
-	
-	posicao = malloc(sizeof(node));
-	temp = head;
-	while(i < index - 1){
-		temp = temp->next;
-		i++;
-	}
-	posicao = temp->next;
-	temp->next = posicao->next;
-	free(posicao);
-	
-}
+void remover(int index);
 
 //adicionar um elemento em dada posicao
-void adicionar(int index, int valor)
-{
-	node *temp, *newnode;
-	int i=0;
-	
-	newnode = malloc(sizeof(node));	
-	temp = head;
-	newnode->data = valor;
-	newnode->next = 0;
-	while(i<index -1){
-		temp = temp->next;
-		i++;
-	}
-	newnode->next = temp->next;
-	temp->next = newnode;
-}
+void adicionar(int index, int valor);
 
 //adiciona um elemento na posicao 0
-void adicionarPrimeiro(int valor)
-{
-	node* temp;
-	
-	temp = malloc(sizeof(node));
-	temp->data = valor;
-	temp->next = head;
-	head = temp;
-}
+void adicionarPrimeiro(int valor);
 
 //adiciona um elemento na ultima posicao
-void adicionarUltimo(int valor)
-{
-	node *temp, *start;
-	
-	temp = malloc(sizeof(node));
-	temp->next = 0;
-	temp->data = valor;
-	start = head;
-	while(start->next != NULL){
-		start = start->next;
-	}
-	start->next = temp;
-}
+void adicionarUltimo(int valor);
 
 //retorna o tamanho da lista
-int lenLinkedList(){
-	node *temp;
-	
-	temp = head;
-	int i=0;
-	while(temp != NULL){
-		i++;
-		temp = temp->next;
-	}
-	
-	return i;
-}
+int lenLinkedList();
 
 //converte a lista para um vetor 
-int* converterVetor(){
-	node *temp = head;
-	int len = lenLinkedList(), i=0;
-	int *vetor = calloc(len, sizeof(int));
-	
-	while(temp != NULL){
-		vetor[i++] = temp->data;
-		temp = temp->next;
-	}
-	
-	return vetor;
-}
+int* converterVetor();
 
 //libera o espaço da list
-void freeList()
-{
-   node* tmp;
-
-   while (head != NULL)
-    {
-       tmp = head;
-       head = head->next;
-       free(tmp);
-    }
-}
+void freeList();
 #endif // LINKEDLIST-OPS_H_INCLUDED
